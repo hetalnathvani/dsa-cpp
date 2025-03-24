e751ff73e82518c989ad37202167082f1a6b807a
Hetal Nathvani <hetal@openxcellinc.com>
2025-03-24 19:07:13 +0530

    Array programs | Find unique and Swap the alternate numbers in array | DONE

---
+++ findUnique.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int ans = 0;
+    int arr[5] = {2, 3, 1, 3, 2};
+    int size = 5;
+
+    for (int i = 0; i < size; i++) {
+        ans = ans^arr[i];
+    }
+
+    return ans;
+}
